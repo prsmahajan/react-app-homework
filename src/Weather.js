c3d6b7a4923e5f3ad7d3751e8bdbd8ddc857cc75
Paras Mahajan <prsmahajan@gmail.com>
2023-03-08 12:13:14 +0530

    initial commit

---
+++ src/Weather.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+export default function Weather() {
+  const [city, setCity] = useState("");
+  const [hasResult, showResult] = useState(false);
+  const [weatherInfo, setWeatherInfo] = useState({});
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    let key = "3a94f3778290bfeee61278505dbbe51d";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+    axios.get(url).then(ShowTemperature);
+  }
+
+  function updateCity(e) {
+    setCity(e.target.value);
+  }
+
+  function ShowTemperature(response) {
+    showResult(true);
+    setWeatherInfo({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description
+    });
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Enter a city..."
+        onChange={updateCity}
+      />
+      <button type="Submit">Search</button>
+    </form>
+  );
+
+  if (hasResult) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weatherInfo.temperature)}°C</li>
+          <li>Description: {weatherInfo.description}</li>
+          <li>Humidity: {weatherInfo.humidity}%</li>
+          <li>Wind: {weatherInfo.wind}km/h</li>
+          <li>
+            <img src={weatherInfo.icon} alt={weatherInfo.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Weather from './Weather';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="Weather">
+      <h1>Weather App</h1>
+      <Weather />
+    </div>
   </React.StrictMode>
 );
 
